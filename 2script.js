51ff3acaf0e11d47ba451f9aa4ccadc47705fc7a
suchka9 <koliayuzkiv9@icloud.com>
2025-04-20 12:34:06 +0200

    Create 2script.js

---
+++ 2script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.nav-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    document.querySelectorAll('.nav-btn').forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+  });
+});
